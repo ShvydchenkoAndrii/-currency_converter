db7a4f20bcefc9c03bb271689e143dbd05b151aa
unknown <shvydchenkoandriy@gmail.com>
2023-03-08 23:17:41 +0200

    bugs in inputs

--- pages/index.js
+++ pages/index.js
@@ -36,12 +36,12 @@ export async function getInfo(currency = null) {
 
 export default function Home() {
   const currencies = ["USD", "UAH", "EUR"];
-  const [usd, setUsd] = useState({});
-  const [eur, setEur] = useState({});
-  const [uah, setUah] = useState({});
+  const [USD, setUSD] = useState({});
+  const [EUR, setEUR] = useState({});
+  const [UAH, setUAH] = useState({});
 
-  const [fromCurrencyValue, setFromCurrencyValue] = useState(undefined);
-  const [toCurrencyValue, setToCurrencyValue] = useState(undefined);
+  const [fromCurrencyValue, setFromCurrencyValue] = useState("");
+  const [toCurrencyValue, setToCurrencyValue] = useState("");
 
   const fromCurrencyInput = useRef(null);
   const toCurrencyInput = useRef(null);
@@ -52,10 +52,11 @@ export default function Home() {
     const fromSelector = fromCurrency.current.value;
     const fromCurrencyNum = fromCurrencyInput.current.value;
     const toSelector = toCurrency.current.value;
+
     const rates = {
-      USD: { UAH: usd.UAH, EUR: usd.EUR },
-      UAH: { USD: uah.USD, EUR: uah.EUR },
-      EUR: { USD: eur.USD, UAH: eur.UAH },
+      USD: { UAH: USD.UAH, EUR: USD.EUR },
+      UAH: { USD: UAH.USD, EUR: UAH.EUR },
+      EUR: { USD: EUR.USD, UAH: EUR.UAH },
     };
     const rate = rates[fromSelector][toSelector] || 1;
     setToCurrencyValue((fromCurrencyNum * rate).toFixed(4));
@@ -65,10 +66,11 @@ export default function Home() {
     const fromSelector = fromCurrency.current.value;
     const toSelector = toCurrency.current.value;
     const toCurrencyNum = toCurrencyInput.current.value;
+
     const rates = {
-      USD: { UAH: usd.UAH, EUR: usd.EUR },
-      UAH: { USD: uah.USD, EUR: uah.EUR },
-      EUR: { USD: eur.USD, UAH: eur.UAH },
+      USD: { UAH: USD.UAH, EUR: USD.EUR },
+      UAH: { USD: UAH.USD, EUR: UAH.EUR },
+      EUR: { USD: EUR.USD, UAH: EUR.UAH },
     };
     const rate = rates[toSelector][fromSelector] || 1;
     setFromCurrencyValue((toCurrencyNum * rate).toFixed(4));
@@ -76,21 +78,21 @@ export default function Home() {
 
   useEffect(() => {
     getInfo("USD").then((res) => {
-      setUsd(res);
+      setUSD(res);
     });
     getInfo("UAH").then((res) => {
-      setUah(res);
+      setUAH(res);
     });
     getInfo("EUR").then((res) => {
-      setEur(res);
+      setEUR(res);
     });
   }, []);
 
   return (
     <>
       <div>
-        <div>{usd.UAH}</div>
-        <div>{eur.UAH}</div>
+        <div>{USD.UAH}</div>
+        <div>{EUR.UAH}</div>
       </div>
       <div>
         <div>
