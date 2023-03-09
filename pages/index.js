5ebbd4ad547cc87ccabca373078bc6f66ae0fb94
unknown <shvydchenkoandriy@gmail.com>
2023-03-09 12:49:17 +0200

    added useEffect to import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "@/components/Header/Header";
 import Converter from "@/components/Converter/Converter";
 import { getInfo } from "@/services/currency_request";
